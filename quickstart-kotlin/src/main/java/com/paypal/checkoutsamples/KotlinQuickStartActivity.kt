1ad8ff86363d490ce6de777418a5d2be1ecc6012
Cody Engel <coengel@paypal.com>
2021-03-30 11:15:08 -0500

    Added Validation To KotlinQuickStartActivity
    * Added validation to KotlinQuickStartActivity to ensure the user has updated the value of PAYPAL_CLIENT_ID, PAYPAL_SECRET, or both depending on requirements for the specific sample.

--- quickstart-kotlin/src/main/java/com/paypal/checkoutsamples/KotlinQuickStartActivity.kt
+++ quickstart-kotlin/src/main/java/com/paypal/checkoutsamples/KotlinQuickStartActivity.kt
@@ -2,26 +2,54 @@ package com.paypal.checkoutsamples
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.paypal.checkoutsamples.order.OrdersQuickStartActivity
 import com.paypal.checkoutsamples.paymentbutton.PaymentButtonQuickStartActivity
 import com.paypal.checkoutsamples.token.TokenQuickStartActivity
 import kotlinx.android.synthetic.main.activity_kotlin_quick_start.*
 
 class KotlinQuickStartActivity : AppCompatActivity() {
+
+    private val clientIdWasUpdated by lazy {
+        PAYPAL_CLIENT_ID != "YOUR-CLIENT-ID-HERE"
+    }
+
+    private val secretWasUpdated by lazy {
+        PAYPAL_SECRET != "ONLY-FOR-QUICKSTART-DO-NOT-INCLUDE-SECRET-IN-CLIENT-SIDE-APPLICATIONS"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_quick_start)
 
         buyWithOrder.setOnClickListener {
-            startActivity(OrdersQuickStartActivity.startIntent(this))
+            if (clientIdWasUpdated) {
+                startActivity(OrdersQuickStartActivity.startIntent(this))
+            } else {
+                displayErrorSnackbar("Please Update PAYPAL_CLIENT_ID In QuickStartConstants.")
+            }
         }
 
         buyWithOrderToken.setOnClickListener {
-            startActivity(TokenQuickStartActivity.startIntent(this))
+            if (clientIdWasUpdated && secretWasUpdated) {
+                startActivity(TokenQuickStartActivity.startIntent(this))
+            } else {
+                displayErrorSnackbar("Please Update PAYPAL_CLIENT_ID and PAYPAL_SECRET In QuickStartConstants.")
+            }
         }
 
         buyWithPaymentButton.setOnClickListener {
-            startActivity(PaymentButtonQuickStartActivity.startIntent(this))
+            if (clientIdWasUpdated) {
+                startActivity(PaymentButtonQuickStartActivity.startIntent(this))
+            } else {
+                displayErrorSnackbar("Please Update PAYPAL_CLIENT_ID In QuickStartConstants.")
+            }
         }
+    }
+
+    private fun displayErrorSnackbar(errorMessage: String) {
+        Snackbar.make(rootQuickStart, errorMessage, Snackbar.LENGTH_INDEFINITE)
+            .apply { setAction("Got It 👍") { dismiss() } }
+            .show()
     }
 }
