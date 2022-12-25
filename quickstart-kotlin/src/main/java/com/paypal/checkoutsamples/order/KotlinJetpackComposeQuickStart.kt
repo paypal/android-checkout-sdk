2cec92064e16418f36a70cfbbb2b08d7602e13c5
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-12-25 23:27:17 +0300

    added jetpack compose sample

---
+++ quickstart-kotlin/src/main/java/com/paypal/checkoutsamples/order/KotlinJetpackComposeQuickStart.kt
@@ -0,0 +1,192 @@
+package com.paypal.checkoutsamples.order
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.approve.OnApprove
+import com.paypal.checkout.cancel.OnCancel
+import com.paypal.checkout.createorder.CreateOrder
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.OrderIntent
+import com.paypal.checkout.createorder.ShippingPreference
+import com.paypal.checkout.createorder.UserAction
+import com.paypal.checkout.error.OnError
+import com.paypal.checkout.order.CaptureOrderResult
+import com.paypal.checkoutsamples.order.usecase.CreateOrderRequest
+import com.paypal.checkoutsamples.order.usecase.CreateOrderUseCase
+import com.paypal.checkoutsamples.ui.theme.AndroidNativeCheckoutSamplesTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+class KotlinJetpackComposeQuickStart : ComponentActivity() {
+    private val checkoutState = MutableStateFlow<PaypalCheckoutState>(Loading)
+    private val checkOutSdk:PayPalCheckout
+        get() =  PayPalCheckout
+    private val createdItems = mutableListOf<CreatedItem>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerPaypalCheckoutCallback(checkOutSdk)
+        val createOrderUseCase by lazy { CreateOrderUseCase() }
+
+
+        setContent {
+            AndroidNativeCheckoutSamplesTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    PaymentScreen(paypalSdkInstance = checkOutSdk, orderUseCase = createOrderUseCase,
+                        checkoutState = checkoutState,
+                        createdItems = createdItems)
+                }
+            }
+        }
+    }
+
+    /* represents the different states to be observed when payment is initiated  */
+    sealed interface PaypalCheckoutState
+    object Loading : PaypalCheckoutState
+    object OrderPaidForSuccessfully : PaypalCheckoutState
+    object OrderCapturingFailed : PaypalCheckoutState
+    object OrderPaymentCancelled : PaypalCheckoutState
+
+    @JvmInline
+    value class OrderPaymentFailedWithAnError(val errorMsg: String) : PaypalCheckoutState
+
+    fun registerPaypalCheckoutCallback(checkoutSdk: PayPalCheckout) {
+        checkoutSdk.registerCallbacks(onApprove = OnApprove { approval ->
+            approval.orderActions.capture { result ->
+                val message = when (result) {
+                    is CaptureOrderResult.Success -> {
+                        checkoutState.update { OrderPaidForSuccessfully }
+                        "payment successful. Thank you for shopping with us"
+                    }
+
+                    is CaptureOrderResult.Error -> {
+                        checkoutState.update { OrderCapturingFailed }
+                        "failed to capture your order"
+                    }
+                }
+                Log.i("ComposeActivityTag",message)
+            }
+        }, onCancel = OnCancel {
+            checkoutState.update { OrderPaymentCancelled }
+            Log.i("ComposeActivityTag","payment cancelled")
+        }, onError = OnError { errorInfo ->
+            checkoutState.update { OrderPaymentFailedWithAnError(errorInfo.reason) }
+            Log.i("ComposeActivityTag","the following error occurred while paying ${errorInfo.reason}")
+        })
+    }
+    @Composable
+    fun PaymentScreen(modifier: Modifier=Modifier,
+                      paypalSdkInstance:PayPalCheckout,
+                      orderUseCase: CreateOrderUseCase,
+                      checkoutState:StateFlow<PaypalCheckoutState>,
+                      createdItems: List<CreatedItem>){
+        var observeCheckoutState by remember { mutableStateOf(false) }
+        var checkoutStateMsg by remember { mutableStateOf("") }
+        val _checkoutState by checkoutState.collectAsState(Loading)
+        when(observeCheckoutState){
+            true->{
+                CircularProgressIndicator(modifier = Modifier.size(200.dp))
+                ObservePaypalCheckoutState(_checkoutState){
+                    observeState,msg->
+                    observeCheckoutState = observeState
+                    checkoutStateMsg=msg
+                }
+            }
+            else->{
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Column(modifier = Modifier, verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally){
+                        Spacer(Modifier.height(20.dp))
+                        AnimatedVisibility(visible = checkoutStateMsg.isNotEmpty()){
+                            Text(checkoutStateMsg, textAlign = TextAlign.Center)
+                        }
+                        Spacer(Modifier.height(20.dp))
+                        Button(modifier=Modifier.fillMaxWidth()
+                            .padding(horizontal = 20.dp),
+                            onClick = {
+                                // initiate payment
+                                observeCheckoutState=true
+                                initiatePaymentMethod(paypalSdkInstance, orderUseCase, createdItems)
+                            }){
+                            Text("Complete Purchase")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @Composable
+    private fun ObservePaypalCheckoutState(checkoutState:PaypalCheckoutState,
+    updateObserveCheckoutState:(Boolean,String)->Unit){
+        LaunchedEffect(checkoutState){
+            when(checkoutState){
+                Loading -> Log.d("ComposeActivityTag","Initiating order capture..")
+                OrderCapturingFailed -> {
+                    // stop observing and show the default view to enable the user to initiate payment again
+                    updateObserveCheckoutState(false,"Order capturing failed..")
+                    Log.d("ComposeActivityTag","Order capturing failed..")
+                }
+                OrderPaidForSuccessfully ->{
+                    Log.d("ComposeActivityTag","Order capturing success")
+                    // stop observing and show the default view to enable the user to initiate payment again
+                    updateObserveCheckoutState(false, "Order capturing success")
+                }
+                OrderPaymentCancelled ->{
+                    Log.d("ComposeActivityTag","Order payment cancelled!")
+                    updateObserveCheckoutState(false,"Order payment cancelled!")
+                }
+                is OrderPaymentFailedWithAnError ->{
+                    updateObserveCheckoutState(false,"Order payment failed with the following error ${checkoutState.errorMsg}")
+                    Log.d("ComposeActivityTag","Order payment failed with the following error ${checkoutState.errorMsg}")
+                }
+            }
+        }
+    }
+    private fun initiatePaymentMethod(
+        paypalSdkInstance: PayPalCheckout,
+        orderUseCase: CreateOrderUseCase,
+        createdItems: List<CreatedItem>
+    ) {
+        // create your order request
+        val createOrderRequest = CreateOrderRequest(
+            orderIntent = OrderIntent.CAPTURE,
+            userAction = UserAction.PAY_NOW, shippingPreference = ShippingPreference.NO_SHIPPING,
+            currencyCode = CurrencyCode.USD, createdItems = createdItems
+        )
+        val orderObject = orderUseCase.execute(createOrderRequest)
+        // start check out process
+        paypalSdkInstance.startCheckout(createOrder = CreateOrder.invoke { actions ->
+            actions.create(orderObject) { orderId ->
+                Log.d("ComposeActivityTag", "Order id $orderId")
+            }
+        })
+    }
+}
